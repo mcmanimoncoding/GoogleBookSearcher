daf0f3c290f0761c74734a5b413e2bf40437afc4
Daniel M <mcmanimoncoding@gmail.com>
2019-07-05 23:22:06 -0700

    commit

---
+++ google-book-searcher/client/build/precache-manifest.f53e2a441e3305dfff2bbbb7d6a5414a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f42c6249d7febc8d7f9a",
+    "url": "/static/js/main.78cca5fb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f42c6249d7febc8d7f9a",
+    "url": "/static/css/main.360d241a.chunk.css"
+  },
+  {
+    "revision": "6059669444805ae4566647bf5fa8b886",
+    "url": "/index.html"
+  }
+];
